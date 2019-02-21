7896ea31b343a60aefbf37a589d1cff47943cda2
ZacharyToney <zach@clearseries.com>
2019-02-21 10:20:01 -0500

    init

---
+++ src/main/java/addTwoNumbers.java
@@ -0,0 +1,8 @@
+public class addTwoNumbers {
+
+    public static int addTwoNumbers(int firstNumber,int secondNumber){
+
+        return (firstNumber + secondNumber);
+    }
+
+}
---
+++ src/test/java/testAddTwoNumbers.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class testAddTwoNumbers {
+
+
+    @Test
+    void testBasic() {
+        Assertions.assertEquals(addTwoNumbers.addTwoNumbers(1,2),3);
+
+    }
+}
